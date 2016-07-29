a15250d7f50858eed36be443dc4a7a972a0fcecd
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-07-29 15:26:43 -0600

    create profile summary model and spec -created method to store data from linkedin API in a ProfileSummary instance

---
+++ app/models/profile_summary.rb
@@ -0,0 +1,12 @@
+class ProfileSummary
+  attr_reader :summary
+
+  def initialize(profile_data)
+    @summary = profile_data["summary"]
+  end
+
+  def self.get_text(user)
+    data = LinkedinService.new.get_profile_information(user.oauth_token)
+    ProfileSummary.new(data)
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :users
+
+  it "gets the user's profile summary from linkedin" do
+    VCR.use_cassette(:profile_summary) do
+      user = users(:erin)
+
+      ps = ProfileSummary.get_text(user)
+      expect(ps.summary[0..14]).to eq "I set extremely"
+    end
+  end
 end
