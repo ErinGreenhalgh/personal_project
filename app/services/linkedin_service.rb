4bc9f6be3709017d227f837d1e95fea35aef8cd8
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-07-29 15:10:48 -0600

    refactor linkedin service method -makes it more flexible for finding not just the summary but other information as necessary -flexible for fetching different format as well

--- app/services/linkedin_service.rb
+++ app/services/linkedin_service.rb
@@ -1,14 +1,12 @@
 class LinkedinService
-  def get_profile_summary
-    # response = Faraday.get("https://api.linkedin.com/v1/people/~:(summary)?format=json")
 
-
-    connection = Faraday.new("https://api.linkedin.com")
-    connection.headers = {"Authorization" => "Bearer AQUYWUPxIxJE-eTwk_4uYVCPx86rUr3xmcMQQ3OUJ0iT8SzAUvnm5VRiiB1wfZD26Iux1sauvEqc0KFAVyu0Kk8F4UBv6tm8wCt29I_Z95qX4cWRE5fMhPVna2F7YFOg7IlHn_4NOkG4qB0Cm3XmUU8OUjE_GzHCB6Fh6jBrQvwYZaBIG4I"}
-
-    response = connection.get("/v1/people/~:(summary)?format=json")
-
-    JSON.parse(response.body)
+  def initialize
+    @connection = Faraday.new("https://api.linkedin.com")
   end
 
+  def get_profile_information(oauth_token, profile_field="summary", format="json")
+    @connection.headers = {"Authorization" => "Bearer #{oauth_token}"}
+    response = @connection.get("/v1/people/~:(#{profile_field})?format=#{format}")
+    JSON.parse(response.body)
+  end
 end
--- spec/services/linkedin_service_spec.rb
+++ spec/services/linkedin_service_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "linkedin service" do
-  VCR.use_cassette("profile_summary") do
-    let(:linkedin_service) { LinkedinService.new }
+  fixtures :users
+  let(:linkedin_service) { LinkedinService.new }
 
-    scenario "returns the user's summary" do
-      data = linkedin_service.get_profile_summary
-      expect(data.count).to eq 1
+  scenario "returns the user's summary" do
+    VCR.use_cassette("profile_summary") do
+      user = users(:erin)
+      data = linkedin_service.get_profile_information(user.oauth_token)
+      expect(data.keys.count).to eq 1
       expect(data["summary"]).to be_truthy
     end
   end
