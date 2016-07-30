1ccbe66ae9104d7cf40227b196946414943a060c
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-07-30 11:03:45 -0600

    delete comments and make user spec pending

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  fixtures :users
-
-  # it "gets the user's profile summary from linkedin" do
-  #   VCR.use_cassette(:profile_summary) do
-  #     user = users(:erin)
-  #
-  #     ps = ProfileSummary.get_text(user)
-  #     expect(ps.summary[0..14]).to eq "I set extremely"
-  #   end
-  # end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
