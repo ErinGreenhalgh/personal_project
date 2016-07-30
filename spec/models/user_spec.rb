70bed502abe54d9ea9225c75d250d0c2b11641f5
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-07-29 22:34:51 -0600

    switch content of user spec to profile summary spec * this test is for the profile summary model, not the user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   fixtures :users
 
-  it "gets the user's profile summary from linkedin" do
-    VCR.use_cassette(:profile_summary) do
-      user = users(:erin)
-
-      ps = ProfileSummary.get_text(user)
-      expect(ps.summary[0..14]).to eq "I set extremely"
-    end
-  end
+  # it "gets the user's profile summary from linkedin" do
+  #   VCR.use_cassette(:profile_summary) do
+  #     user = users(:erin)
+  #
+  #     ps = ProfileSummary.get_text(user)
+  #     expect(ps.summary[0..14]).to eq "I set extremely"
+  #   end
+  # end
 end
