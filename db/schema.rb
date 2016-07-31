89613ba7582aedaa52fd39314a08f4361a6587fe
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-07-31 12:19:22 -0600

    create migration to add timestamps to profile summaries table

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160730033910) do
+ActiveRecord::Schema.define(version: 20160731172339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "profile_summaries", force: :cascade do |t|
-    t.text    "content"
-    t.integer "user_id"
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_profile_summaries_on_user_id", using: :btree
   end
 
