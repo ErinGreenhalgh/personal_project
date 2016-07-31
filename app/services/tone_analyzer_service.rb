1d457e9f58a0de594a38bd3e0b83087554e8a503
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-07-31 12:18:23 -0600

    add caching to tone analyzer service * wrapped API call in a cache method with the cache key being the updated_at of the user's profile summary; this will ensure that when a draft(down the line) is edited, a new API call will be made to generate another report * this method needs refactoring

--- app/services/tone_analyzer_service.rb
+++ app/services/tone_analyzer_service.rb
@@ -1,17 +1,15 @@
 class ToneAnalyzerService
-  # attr_reader :connection
-
-  def initialze
-  end
 
   def analyze_text(user)
-    connection = Faraday.new("https://gateway.watsonplatform.net")
-    connection.params = {version: "2016-05-19", text: "#{user.profile_summary.content}"}
-    connection.headers = {"password"      => ENV["TONE_ANALYZER_PASSWORD"],
-                          "username"      => ENV["TONE_ANALYZER_USERNAME"],
-                          "Authorization" => "Basic #{ENV['TONE_ANALYZER_AUTHORIZATION']}"}
-    response = connection.get("/tone-analyzer/api/v3/tone")
-    JSON.parse(response.body)
+    Rails.cache.fetch("#{user.profile_summary.updated_at}-tone-report") do
+      connection = Faraday.new("https://gateway.watsonplatform.net")
+      connection.params = {version: "2016-05-19", text: "#{user.profile_summary.content}"}
+      connection.headers = {"password"      => ENV["TONE_ANALYZER_PASSWORD"],
+                            "username"      => ENV["TONE_ANALYZER_USERNAME"],
+                            "Authorization" => "Basic #{ENV['TONE_ANALYZER_AUTHORIZATION']}"}
+      response = connection.get("/tone-analyzer/api/v3/tone")
+      JSON.parse(response.body)
+    end
   end
 
 end
