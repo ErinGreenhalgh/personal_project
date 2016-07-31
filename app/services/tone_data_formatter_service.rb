2589b10326a4fd06c707d80e746cf8a41d59a4a3
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-07-31 12:20:34 -0600

    create tone data formatter service * takes in the data from the tone analyzer service and formats the data so that hashes can be easily fed to create methods for EmotionTone, LanguageTone, and SocialTone objects

---
+++ app/services/tone_data_formatter_service.rb
@@ -0,0 +1,44 @@
+class ToneDataFormatterService
+  def service
+    ToneAnalyzerService.new
+  end
+
+  def categories(user)
+    data = service.analyze_text(user)
+    data["document_tone"]["tone_categories"]
+  end
+
+  def emotion_tones(user)
+    categories(user)[0]
+  end
+
+  def language_tones(user)
+    categories(user)[1]
+  end
+
+  def social_tones(user)
+    categories(user)[2]
+  end
+
+  def round(score)
+    (score * 1000).to_i
+  end
+
+  def format_attributes(tones)
+    tones.map do |tone|
+      [tone["tone_id"], round(tone["score"])]
+    end.to_h
+  end
+
+  def emotion_attributes(user)
+    format_attributes(emotion_tones(user)["tones"])
+  end
+
+  def language_attributes(user)
+    format_attributes(language_tones(user)["tones"])
+  end
+
+  def social_attributes(user)
+    format_attributes(social_tones(user)["tones"])
+  end
+end
---
+++ spec/services/tone_data_formatter_service_spec.rb
@@ -0,0 +1,45 @@
+require "rails_helper"
+
+RSpec.describe "tone data formatter service" do
+  fixtures :users, :profile_summaries
+
+  let(:user) { users(:erin) }
+  let(:formatter) { ToneDataFormatterService.new }
+
+  it "isolates an array of tone categories" do
+    VCR.use_cassette("tone_report") do
+      expect(formatter.categories(user).count).to eq 3
+    end
+  end
+
+  it "finds tones by category" do
+      expect(formatter.emotion_tones(user)["category_id"]).to eq "emotion_tone"
+      expect(formatter.language_tones(user)["category_id"]).to eq "language_tone"
+      expect(formatter.social_tones(user)["category_id"]).to eq "social_tone"
+  end
+
+  it "finds emotion attributes" do
+    attributes = {"anger"   => 429,
+                  "disgust" => 231,
+                  "fear"    => 288,
+                  "joy"     => 28,
+                  "sadness" => 620}
+    expect(formatter.emotion_attributes(user)).to eq attributes
+  end
+
+  it "finds language attributes" do
+    attributes = {"analytical" => 0,
+                  "confident"  => 0,
+                  "tentative"  => 715}
+    expect(formatter.language_attributes(user)).to eq attributes
+  end
+
+  it "finds social attributes" do
+    attributes = {"openness_big5"          => 190,
+                  "conscientiousness_big5" => 66,
+                  "extraversion_big5"      => 904,
+                  "agreeableness_big5"     => 320,
+                  "emotional_range_big5"   => 489}
+    expect(formatter.social_attributes(user)).to eq attributes
+  end
+end
