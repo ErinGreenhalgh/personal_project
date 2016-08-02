961378b362c33c39cbd924e002c621a8de944ed0
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-08-02 17:03:19 -0600

    update relationships in models

--- app/models/profile_summary.rb
+++ app/models/profile_summary.rb
@@ -1,5 +1,6 @@
 class ProfileSummary < ActiveRecord::Base
   belongs_to :user
+  has_many :reports
 
   def self.get_text(user)
     data = LinkedinService.new.get_profile_information(user.oauth_token)
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,3 +1,6 @@
 class Report < ApplicationRecord
   belongs_to :profile_summary
+  has_one :emotion_tone
+  has_one :language_tone
+  has_one :social_tone
 end
